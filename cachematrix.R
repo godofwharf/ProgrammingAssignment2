187904b0a1e6194e63912246635176e8911dd22d
Guruprasad Sridharan <guruprasad.sridharan@gmail.com>
2014-05-22 17:05:10 +0530

    Changes to cachematrix.R
    Added two functions to cachematrix.R namely
    1. makeCacheMatrix
    2. cacheSolve
    
    Added relevant comments
    
    Tested changes on local machine

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## makeCacheMatrix function creates a special matrix object which is a list 
+## of functions namely set, get, setInv, getInv
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inv <- NULL
+        ## Function used to set the matrix 'x'
+        set <- function(y) {
+                x <<- y
+                inv <<- NULL
+        }
+        ## Function that returns the matrix 'x'
+        get <- function() x
+        ## Function used to set the inverse for matrix 'x'
+        setInv <- function(i) inv <<- i
+        ## Function that returns the inverse for matrix 'x'
+        getInv <- function() inv
+        ## Returns a list of functions
+        list(set = set, get = get, setInv = setInv, getInv = getInv)
 }
 
 
-## Write a short comment describing this function
+## CacheSolve function takes a special matrix object and a variable
+## number of arguments as input and returns an inverse of the matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        ## Checks if inverse is computed already for 'x'
+        inv <- x$getInv()
+        if(!is.null(inv)) {
+                ## Inverse has been computed already
+                print("Cache hit successful")
+                ## Returns the stored inverse matrix
+                return(inv)
+        }
+        ## Gets the matrix object associated with 'x' 
+        mat <- x$get()
+        ## Compute inverse for 'mat'
+        inv <- solve(mat, ...)
+        ## Store inverse for 'mat' in 'x'
+        x$setInv(inv)
+        ## Return the inverse of 'x'
+        inv
 }
+
